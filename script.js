bffddddc70241bfcf8695a51fdf77b2762262bc8
m0x0m0x <mx0mx@tuta.io>
2024-03-15 22:54:51 +0530

    flighradar test

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+/**
+ * This is for testing API Calls basically
+ */
+
+// import axios from "axios";
 console.log("hello");
+
+let config = {
+  method: "get",
+  maxBodyLength: Infinity,
+  url: "https://fr24api.flightradar24.com/api/airlines/afl",
+  headers: {
+    Accept: "application/json",
+    Authorization: "Bearer <token>",
+  },
+};
+
+axios
+  .request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
